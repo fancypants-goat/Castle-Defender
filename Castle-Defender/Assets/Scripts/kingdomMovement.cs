983ab634144965c96567ccb7b04d492afd9a3663
RehanAtron <rehanahmed9574@gmail.com>
2024-07-09 21:21:57 +0500

    Kingdom build

---
+++ Castle-Defender/Assets/Scripts/kingdomMovement.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class kingdomMovement : MonoBehaviour
+{
+    
+    [SerializeField] float maxmoveSpeed;
+    private Transform tf;
+    private Vector2 movement;
+    void Start()
+    {
+        tf = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+    void Move()
+    {
+        //Inputs
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        //movement
+        float currentSpeedX = maxmoveSpeed * movement.x * Time.deltaTime;
+        float currentSpeedY = maxmoveSpeed * movement.y * Time.deltaTime;
+        tf.position = tf.position + new Vector3(currentSpeedX,currentSpeedY);
+    }
+}
