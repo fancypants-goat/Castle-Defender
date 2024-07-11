23ddb81c552f60b76f958bb14bb5705f2722be6b
RehanAtron <rehanahmed9574@gmail.com>
2024-07-11 21:58:53 +0500

    worker

---
+++ Castle-Defender/Assets/Resource.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Resource : MonoBehaviour
+{
+    public Vector3 target;
+    private WorkerManager workerManager;
+
+    void Start() 
+    {
+        workerManager = FindObjectOfType<WorkerManager>();
+    }
+    void OnMouseDown() 
+    {
+        target = transform.position;
+        workerManager.target = target; 
+        workerManager.shouldMove = true;
+        StartCoroutine(ClearList());
+    }
+    IEnumerator ClearList()
+    {
+        yield return null;
+        workerManager.selectedWorkers.Clear();
+        workerManager.shouldMove = false;
+    }
+}
