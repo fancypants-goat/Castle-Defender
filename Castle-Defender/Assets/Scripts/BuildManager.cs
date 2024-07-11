c9595c0c74753eb5288abeef118745f65bd5f181
fancypants <edingmichiel1@gmail.com>
2024-07-11 15:53:04 +0200

    improved building kingdom
    improved the building of kingdom mechanic
    
    cursor now deactivates if held over an expansion

--- Castle-Defender/Assets/Scripts/BuildManager.cs
+++ Castle-Defender/Assets/Scripts/BuildManager.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BuildManager : MonoBehaviour
@@ -10,16 +13,42 @@ public class BuildManager : MonoBehaviour
     [SerializeField] private GameObject expansion;
     [SerializeField] private Transform kingdom;
     [SerializeField] private bool isBuilding;
+    [SerializeField] private bool canBuildOnSelectedGridPosition;
     private float cooldown;
-    public HashSet<Vector3> usedSpaces = new() {
-        new(0,0,0),
+    public HashSet<Expansion> expansions = new() {
+        new Expansion(Vector3.zero),
     };
-    public HashSet<Vector3> usableSpaces = new() {
-        new(1,0,0),
-        new(-1,0,0),
-        new (0,1,0),
-        new(0,-1,0)
-    };
+    public bool ExpansionsContains (Vector3 position) {
+        foreach (Expansion expansion in expansions) {
+            if (expansion.position == position) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+    public bool ExpansionsContains (params Vector3[] positions) {
+        foreach (Expansion expansion in expansions) {
+            if (positions.Contains(expansion.position)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+    public bool CheckForNeighbourExpansions (Vector3 position) {
+        bool nextToExpansion = ExpansionsContains
+        (
+            position + Vector3.up,
+            position + Vector3.down,
+            position + Vector3.left,
+            position + Vector3.right
+        );
+
+        return nextToExpansion;
+    }
+
+    
     private Vector3 mousePos;
     private Vector3 relativeMousePos;
     void Update() 
@@ -33,71 +62,61 @@ public class BuildManager : MonoBehaviour
             mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             mousePos.z = 0;
         }
-        // set the position of cursor
-        cursor.transform.position = mousePos;
-        CursorColorCheck();
-        SnapCursorToGridPosition();
-        // Building Function
-        Building();
+        
+        if (isBuilding) {    
+            // calculates the difference between mouse and main kingdom
+            relativeMousePos = new Vector3 (Mathf.RoundToInt(mousePos.x - kingdom.position.x),Mathf.RoundToInt(mousePos.y - kingdom.position.y),0);
+
+            CheckIfCanBuildOnGridPosition();
+            CursorColorCheck();
+
+            SnapCursorToGridPosition();
+            // Building Function
+            Building();
+        }
     }
 
     public void Build()
     {
         // Switches Build Mode
         isBuilding = !isBuilding;
+
+        // (de)activate the cursor depending on isBuilding
+        cursor.SetActive(isBuilding);
     }
     private void Building()
     {
-        if (isBuilding)
+        if (!isBuilding) return;
+
+        // set the position of this object to mousePos
+        transform.position = mousePos;
+
+        // removing the time between this frame and last frame from cooldown
+         cooldown -= Time.deltaTime;
+
+        // if the left mouse button is pressed and cooldown is less then or equals to 0
+        if (Input.GetMouseButton(0) && cooldown <= 0 && canBuildOnSelectedGridPosition)
         {
-            // set the position of this object to mousePos
-            transform.position = mousePos;
-
-            // activating this object
-            cursor.SetActive(true);
-
-            // removing the time between this frame and last frame from cooldown
-            cooldown -= Time.deltaTime;
-
-            // calculates the difference between mouse and main kingdom
-            relativeMousePos = new Vector3 (Mathf.RoundToInt(mousePos.x - kingdom.position.x),Mathf.RoundToInt(mousePos.y - kingdom.position.y),0);
-
-            // if the left mouse button is pressed and cooldown is less then or equals to 0
-            if(Input.GetMouseButton(0) && cooldown <= 0 && usableSpaces.Contains(relativeMousePos)&& !usedSpaces.Contains(relativeMousePos))
-            {
-                // creating a new expansion at the position of the cursor
-                // this also sticks the expansion to a grid using Mathf.RoundToInt()
-                Instantiate(expansion,kingdom.position + relativeMousePos,Quaternion.identity,kingdom);
-                // adds relative mouse position to list
-                AddNewUsableSpaces(relativeMousePos);
-                // resetting the cooldown
-                cooldown = 0.2f;
-            }
-        }
-        else
-        {
-            // deactivating the cursor object
-            cursor.SetActive(false);
+            // creating a new expansion at the position of the cursor
+            // this also sticks the expansion to a grid using Mathf.RoundToInt()
+            Instantiate(expansion, kingdom.position + relativeMousePos, Quaternion.identity, kingdom);
+            // adds relative mouse position to list
+            Expansion expansionData = new(relativeMousePos);
+            AddNewUsableSpaces(expansionData);
+            // resetting the cooldown
+            cooldown = 0.2f;
         }
     }
-    private void AddNewUsableSpaces(Vector3 current)
+
+
+    private void AddNewUsableSpaces (Expansion current)
     {
-        usedSpaces.Add(current);
-        // adds values to the list
-        usableSpaces.Add(new Vector3 (current.x + 1,current.y,0));
-        usableSpaces.Add(new Vector3 (current.x - 1,current.y,0));
-        usableSpaces.Add(new Vector3 (current.x,current.y + 1,0));
-        usableSpaces.Add(new Vector3 (current.x,current.y - 1,0));
-        usableSpaces.Remove(current);
-        if (usableSpaces.Contains(new Vector3(0,0,0)))
-        {
-            usableSpaces.Remove(new Vector3(0,0,0));
-        }
+        expansions.Add(current);
     }
 
     void CursorColorCheck()
     {
-        if (usableSpaces.Contains(relativeMousePos) && !usedSpaces.Contains(relativeMousePos))
+        if (canBuildOnSelectedGridPosition)
         {
             cursorSpriteRenderer.color = Color.green;
         }
@@ -106,7 +125,33 @@ public class BuildManager : MonoBehaviour
             cursorSpriteRenderer.color = Color.red;
         }
     }
+    void CheckIfCanBuildOnGridPosition() {
+        bool expansionOnThisGridPosition = ExpansionsContains(relativeMousePos);
+        canBuildOnSelectedGridPosition = !expansionOnThisGridPosition && CheckForNeighbourExpansions(relativeMousePos);
+
+        if (!ExpansionsContains(relativeMousePos)) {
+            cursor.SetActive(true);
+        }
+        else {
+            cursor.SetActive(false);
+        }
+    }
     void SnapCursorToGridPosition() {
-        cursor.transform.position = new Vector3 (Mathf.RoundToInt(mousePos.x - kingdom.position.x),Mathf.RoundToInt(mousePos.y - kingdom.position.y),0);
+        cursor.transform.position = kingdom.position + relativeMousePos;
+    }
+}
+
+
+[System.Serializable]
+public class Expansion
+{
+    public Vector3 position;
+
+    public Expansion() {
+
+    }
+
+    public Expansion (Vector3 position) {
+        this.position = position;
     }
 }
