0f77c00f313eea1cf54199730b71753f3f06abd7
fancypants <edingmichiel1@gmail.com>
2024-07-13 14:54:03 +0200

    improved camera zoom

--- Castle-Defender/Assets/Scripts/CameraZoomManager.cs
+++ Castle-Defender/Assets/Scripts/CameraZoomManager.cs
@@ -4,24 +4,9 @@ using UnityEngine;
 
 public class CameraZoomManager : MonoBehaviour
 {
-    // this is the amount that the camera zooms in/out per scroll
-    public float amountPerZoom;
-
-    new private Camera camera;
-
-    void Start() {
-        camera = GetComponent<Camera>();
-    }
     // Update is called once per frame
     void Update()
     {
-        if (camera.orthographicSize >= 2)
-        {
-            camera.orthographicSize -= amountPerZoom * Input.GetAxis("Mouse ScrollWheel");
-        }
-        else
-        {
-            camera.orthographicSize = 2;
-        }
+
     }
 }
---
+++ Castle-Defender/Assets/Scripts/ExpansionObject.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExpansionObject : MonoBehaviour
+{
+    public float zoomSpeed;
+    public float zoomVelocity;
+
+    private Camera KingdomCamera;
+
+    new private Renderer renderer;
+
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        KingdomCamera = GameObject.Find("KingdomCamera").GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        if (!IsObjectFullyInView()) {
+            zoomVelocity += 0.0008f;
+        }
+        else {
+            zoomVelocity -= 0.0002f;
+        }
+
+        if (zoomVelocity < 0) {
+            zoomVelocity = 0;
+        }
+        if (zoomVelocity > 0.05f) {
+            zoomVelocity = 0.05f;
+        }
+
+        KingdomCamera.orthographicSize += zoomVelocity;
+        Camera.main.orthographicSize = KingdomCamera.orthographicSize + 2;
+    }
+
+
+    bool IsExpansionPartiallyInView() {
+        Plane[] planes = GeometryUtility.CalculateFrustumPlanes(KingdomCamera);
+        Bounds bounds = renderer.bounds;
+
+        return GeometryUtility.TestPlanesAABB(planes, bounds);
+    }
+    bool IsObjectFullyInView()
+    {
+        if (renderer == null || KingdomCamera == null)
+        {
+            return false;
+        }
+
+        Vector3[] corners = new Vector3[8];
+        Bounds bounds = renderer.bounds;
+
+        corners[0] = bounds.min;
+        corners[1] = new Vector3(bounds.min.x, bounds.min.y, bounds.max.z);
+        corners[2] = new Vector3(bounds.min.x, bounds.max.y, bounds.min.z);
+        corners[3] = new Vector3(bounds.max.x, bounds.min.y, bounds.min.z);
+        corners[4] = new Vector3(bounds.max.x, bounds.max.y, bounds.min.z);
+        corners[5] = new Vector3(bounds.max.x, bounds.min.y, bounds.max.z);
+        corners[6] = new Vector3(bounds.min.x, bounds.max.y, bounds.max.z);
+        corners[7] = bounds.max;
+
+        foreach (var corner in corners)
+        {
+            Vector3 viewportPoint = KingdomCamera.WorldToViewportPoint(corner);
+            if (viewportPoint.x < 0 || viewportPoint.x > 1 || viewportPoint.y < 0 || viewportPoint.y > 1 || viewportPoint.z < 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
